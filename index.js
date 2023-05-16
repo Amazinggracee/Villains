99f3ba236cb65ab0b880df2296040dfdae491ca7
AMARACHI DIMKPA <amaradimkpa1@gmail.com>
2023-05-16 04:49:05 +0100

    Add alert

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const supervillains = require('supervillains');
 supervillains.all;
 
 const hello = supervillains.random();
-console.log("I am your worst nightmare," + " " +hello);
+alert("I am your worst nightmare," + " " +hello);
